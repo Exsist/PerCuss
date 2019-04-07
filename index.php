56fc69864b5814c2c6ccb5c8ae7f65cd7a09d492
DexNick4501 <idesignerdisk2@yandex.ru>
2019-04-07 15:41:33 +0500

    create base of RESTful API

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
+//phpinfo();
 
+$page = (isset($_GET['page'])) ? $_GET['page']  : '';
+
+if (isset($page)) {
+    if ($page == 'main') {
+        echo "this is $page page";
+    }
+    if ($page == 'description') {
+        echo "this is $page page";
+    }
+    if ($page == 'albums') {
+        echo "this is $page page";
+    }
+    if ($page == 'capability') {
+        echo "this is $page page";
+    }
+} else {
+    echo "this is main page";
+}
