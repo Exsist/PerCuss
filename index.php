90e433e3449064c9b0ffe734ba6b124b02581b89
DexNick4501 <idesignerdisk2@yandex.ru>
2019-04-07 22:07:27 +0500

    desktop (full-hd) layout site add:  -background (position, url, repeat and size)

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 //$connect = mysqli_connect('localhost', 'root', '', 'percuss');
 
 include 'tpl/header.html';
+$path = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+if ($path) { // TODO: СДЕЛАТЬ РАЗЛИЧНЫЕ ЗАПРОСЫ
+}
 
 $page = (isset($_GET['page'])) ? $_GET['page']  : ''; // TODO: Переписать механизм подключения других страниц.
 
